d6231e45157dc16199f592093323ad9c12385077
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-09-06 08:55:17 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+/**
+ * IMPORTS
+ */
+import {React} from 'react';
+
+
+/**
+ * CODE
+ */
+
+/**
+ * I am the main application element.
+ *
+ * :returns: main application JSX
+ */
+function App ()
+{
+  return (
+    <h1>
+      Pokémon TCG
+    </h1>
+  );
+}
+
+
+/**
+ * EXPORTS
+ */
+export {
+  App
+};
