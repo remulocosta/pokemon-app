72d56c028989b28c3ace217fa289821b99faaab3
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-09-07 03:01:51 -0300

    Add eslint configuration files

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: all,
+  arrowParens: avoid,
+  bracketSpacing: true,
+  fluid: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  bracketSameLine: true,
+}
