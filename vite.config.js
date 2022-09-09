0f40d57ca42829810b4b58ada20c4375b9d51fb5
Remulo Costa <remulo.costa@vulcanet.com.br>
2022-09-09 04:04:35 -0300

    Create PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,59 @@ import react from '@vitejs/plugin-react';
 import {resolve} from 'path';
 import {defineConfig} from 'vite';
 import eslint from 'vite-plugin-eslint';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     eslint(),
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,jpg,png,svg,json}'],
+        sourcemap: true
+      },
+      devOptions: {
+        enabled: false
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        'theme_color': '#f1f9fe',
+        'background_color': '#f1f9fe',
+        'display': 'fullscreen',
+        'orientation': 'portrait',
+        'id': '/',
+        'scope': '/',
+        'start_url': '/index.html',
+        'name': 'Pok\u00e9mon TCG App',
+        'short_name': 'pokemon-app',
+        'description': 'Pok\u00e9mon TCG FrontEnd Challenge',
+        'icons': [
+          {
+            'src': '/assets/icons/icon-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/icons/icon-256x256.png',
+            'sizes': '256x256',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/icons/icon-384x384.png',
+            'sizes': '384x384',
+            'type': 'image/png'
+          },
+          {
+            'src': '/assets/icons/icon-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
